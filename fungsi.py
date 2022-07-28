0977366771d7a1232fa2545bab6b65c14d44b8b1
biru <Biru@gmail.com>
2022-07-28 11:50:57 +0700

    latihan fungsi file pisah

--- fungsi.py
+++ fungsi.py
@@ -1,21 +1,8 @@
 #latiahan fungsi
 #penambahan if
 
-def biasa(tipe, jam='1'):  # argumen  || catatan dalam pemberian nilai default ditaruh sebelah kanan agar tidak terjadi error
-    c = 0
-    jami = int(jam)
-    if tipe == 'mobil':
-        c = jami * 2000
-    elif tipe == 'motor':
-        c = jami * 1000
-    else:
-        c = 7500
-    return c
-
-
-def langganan(saldo):
-    sisa = saldo - 5000
-    return sisa
+from biasaaja import biasa
+from langgananf import langganan
 
 
 gol = input("masukan gologan : ")
---
+++ langgananf.py
@@ -0,0 +1,3 @@
+def langganan(saldo):
+    sisa = saldo - 5000
+    return sisa
