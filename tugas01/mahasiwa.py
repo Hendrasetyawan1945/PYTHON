a0a5f51c8c16f34e2a5fd7860f390dc76dad3d02
biru <Biru@gmail.com>
2022-08-02 14:25:41 +0700

    tugas done

--- tugas01/mahasiwa.py
+++ tugas01/mahasiwa.py
@@ -1,38 +1,60 @@
-from pesertadidik import Mahasiswa 
-
+from pesertadidik import Mahasiswa
 class MahasiswaS1(Mahasiswa):
-    def __init__(self, nama, alamat, nim,semester,skripsi):
+    status = "S1"
+    def __init__(self, nama, alamat, nim, semester, skripsi):
         self.skripsi = skripsi
-        super().__init__(nama, alamat,nim,semester)
-       
+        super().__init__(nama, alamat, nim, semester)
 
     def __str__(self):
-        return '{} || {} || {} || {} || {}'.format(self.nama, self.alamat, self.nim,self.semester,self.skripsi)
+        return '{} || {} || {} || {} || {} ||  '.format(self.nama, self.alamat, self.nim, self.semester, self.status)
+
+    @classmethod
+    def matkul(cls, matkul):
+        cls.matkul = matkul
+
+    @classmethod
+    def ketmatkul(cls):
+        return 'Mengambil matakuliah {}'.format(cls.matkul)
 
 
 class MahasiswaS2(Mahasiswa):
-    def __init__(self, nama, alamat, nim, semester, tesis,spp):
+    status = "S2"
+    def __init__(self, nama, alamat, nim, semester, tesis, spp):
         self.tesis = tesis
         self.spp = spp
         super().__init__(nama, alamat, nim, semester)
 
     def __str__(self):
-        return '{} || {} || {} || {} || {} || {}'.format(self.nama, self.alamat, self.nim, self.semester, self.tesis,self.spp)
+        return '{} || {} || {} || {} || {} || {} || {}'.format(self.nama, self.alamat, self.nim, self.semester, self.tesis, self.spp,self.status)
 
 
 class MahasiswaS3(Mahasiswa):
-    def __init__(self, nama, alamat, nim, semester,penelitian):
+    status = "S2"
+    def __init__(self, nama, alamat, nim, semester, penelitian):
         self.penelitian = penelitian
         self.gelar = "Doktor"
         super().__init__(nama, alamat, nim, semester)
 
     def __str__(self):
-        return '{} || {} || {} || {} || {} || {}'.format(self.nama, self.alamat, self.nim, self.semester, self.penelitian,self.gelar)
+        return '{} || {} || {} || {} || {} || {}'.format(self.nama, self.alamat, self.nim, self.semester, self.penelitian, self.gelar, self.status)
+
+    def kampus(self, kuliah):
+        self.kuliah = kuliah
+
+    def ketkampus(self):
+        return 'Sedang Kuliah di {}'.format(self.kuliah)
 
 
-alfida = MahasiswaS1('Alfida','Magelang','2021075','8','Sistem Gis')    
+alfida = MahasiswaS1('Alfida', 'Magelang', '2021075', '8', 'Sistem Gis')
 print(alfida)
-annas = MahasiswaS2('Annas', 'Banten', '2018071', '4', 'Pemetaan tataruang kota','6000000')
+MahasiswaS1.matkul("Data sains")
+print(MahasiswaS1.ketmatkul())
+
+annas = MahasiswaS2('Annas', 'Banten', '2018071', '4',
+                    'Pemetaan tataruang kota', '6000000')
 print(annas)
-biruu = MahasiswaS3('Biru', 'Tulungagung', '2018071', '5', 'Pemetaan tatan Global')
+biruu = MahasiswaS3('Biru', 'Tulungagung', '2018071',
+                    '5', 'Pemetaan tatan Global')
 print(biruu)
+biruu.kampus("ITB")
+print(biruu.ketkampus())
--- tugas01/pegawai.py
+++ tugas01/pegawai.py
@@ -1,37 +1,37 @@
 from staf import Staff
 
 class Dosen(Staff):
+    jabatan = "Rektor"
     def __init__(self, nama, alamat,nip):
         self.nip = nip
-        self.gaji = 5000000
         super().__init__(nama,alamat)
      
     def __str__(self):
-        return '{} || {} || {} || {} '.format(self.nama, self.alamat, self.nip,self.gaji)
+        return '{} || {} || {} || {} '.format(self.nama, self.jabatan,self.alamat, self.nip)
 
 
 class Admin(Staff):
-    def __init__(self, nama, alamat, nipA,gaji):
+    jabatan = "Admin keuangan"
+    def __init__(self, nama, alamat, nipA):
         self.nipA = nipA
-        self.gajiA = gaji
         super().__init__(nama, alamat)
 
     def __str__(self):
-        return '{} || {} || {} || {} '.format(self.nama, self.alamat, self.nipA, self.gajiA)
+        return '{} || {} || {} || {} '.format(self.nama, self.jabatan, self.alamat, self.nipA)
     
 
 class CleaningServis(Staff):
-    gaji = 3500000
+    jabatan = "Staff Elit Kebersian Dunia"
     def __init__(self, nama, alamat, nipCS):
         self.nipCS = nipCS
         super().__init__(nama, alamat)
 
     def __str__(self):
-        return '{} || {} || {} || {} '.format(self.nama, self.alamat, self.nipCS, self.gaji)
+        return '{} || {} || {} || {} '.format(self.nama,self.jabatan,self.alamat, self.nipCS)
     
 bambang = Dosen('Bambang','Tegal','987836')
 print(bambang) 
-anisa = Admin('Anisa', 'Aceh', '900006',4500000)
+anisa = Admin('Anisa', 'Aceh', '900006')
 print(anisa)
 sukarmi = CleaningServis('sukarmi','Sulawesi','679972')
 print(sukarmi)
